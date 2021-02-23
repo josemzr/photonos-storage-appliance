56ab489e28eb5ed0a1a8ea01a07e90ea0d9d222b
Jose Manzaneque Ramos <vaulner@gmail.com>
2021-02-23 14:51:51 +0100

    Update photon-settings.sh

--- scripts/photon-settings.sh
+++ scripts/photon-settings.sh
@@ -24,6 +24,26 @@ tdnf install -y \
   tar \
   nfs-utils
 
+echo '> Configuring Minio...'
+useradd --system minio-user --shell /sbin/nologin
+groupadd minio-user
+curl -O https://dl.minio.io/server/minio/release/linux-amd64/minio
+mv minio /usr/local/bin
+chmod +x /usr/local/bin/minio
+chown minio-user:minio-user /usr/local/bin/minio
+
+mkdir /etc/minio
+chown minio-user:minio-user /etc/minio
+chown minio-user:minio-user /mnt/s3
+
+echo "MINIO_VOLUMES="/mnt/s3"" > /etc/default/minio
+echo "MINIO_OPTS="-C /etc/minio"" >> /etc/default/minio
+
+# Configure MinIO Systemd Service
+setcap 'cap_net_bind_service=+ep' /usr/local/bin/minio
+curl -O https://raw.githubusercontent.com/minio/minio-service/master/linux-systemd/minio.service
+mv minio.service /etc/systemd/system
+
 echo '> Enable Docker in Systemd'
 systemctl enable docker
 
