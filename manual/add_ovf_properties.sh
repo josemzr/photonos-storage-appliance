c677e6abc35adbe2bd5ac307a44dab46b8e85860
Jose Manzaneque Ramos <vaulner@gmail.com>
2021-02-23 13:33:48 +0100

    Update add_ovf_properties.sh

--- manual/add_ovf_properties.sh
+++ manual/add_ovf_properties.sh
@@ -21,6 +21,7 @@ else
     sed -i '/^      <vmw:ExtraConfig ovf:required="false" vmw:key="nvram".*$/d' ${OVF_PATH}/${PHOTON_APPLIANCE_NAME}.ovf
     sed -i "/^    <File ovf:href=\"${PHOTON_APPLIANCE_NAME}-file1.nvram\".*$/d" ${OVF_PATH}/${PHOTON_APPLIANCE_NAME}.ovf
     sed -i 's#ovf:capacity="1"#ovf:capacity="${disk2size}"#g' ${OVF_PATH}/${PHOTON_APPLIANCE_NAME}.ovf
+    sed -i 's#ovf:capacity="2"#ovf:capacity="${disk2size2}"#g' ${OVF_PATH}/${PHOTON_APPLIANCE_NAME}.ovf
     sed -i 's/ovf:fileRef="file2"//g' ${OVF_PATH}/${PHOTON_APPLIANCE_NAME}.ovf
     sed -i '/vmw:ExtraConfig.*/d' ${OVF_PATH}/${PHOTON_APPLIANCE_NAME}.ovf
 fi
